0808e633dc23ebd3bcf0cef06c178382a1ea3f7e
igorkukric <you@example.com>
2023-08-25 23:44:12 +0200

    Create movie card html template

--- app.js
+++ app.js
@@ -24,8 +24,45 @@ async function FetchData(url) {
 
 // Fetch and show results based on url
 async function fetchAndShowResult(url) {
-    const data = await FetchData(url)
-    if(data && data.result) {
-        showResults(data.results)
-    }
+  const data = await FetchData(url);
+  if (data && data.result) {
+    showResults(data.results);
+  }
+}
+
+// Create movie card html template
+function createMovieCard(movie) {
+  const { posterPath, originalTitle, releaseDate, overview } = movie;
+  const imagePath = posterPath ? imgApi + posterPath : "./img-01.jpeg";
+  const truncatedTitle =
+    originalTitle.length > 15
+      ? originalTitle.slice(0, 15) + "..."
+      : originalTitle;
+  const formattedDate = releaseDate || "No release date";
+  const cardTemplate = `
+        <div class='column'>
+            <div class='card'>
+                <a class='card-media' href='img-01.jpeg'>
+                    <img src='${imagePath}' alt='${originalTitle}'
+                    width='100%' />
+                </a>
+            <div class='card-content'>
+                <div class='card-header'>
+                    <div class='left-content'>
+                        <h3 style='font-weight: 600'>${truncatedTitle}
+                        <span style='color: #12efec'>${formattedDate}</span>
+                    </div>
+                    <div class='right-content'>
+                        <a href='${imagePath}' target='_blank'
+                        class='card-btn'>See Cover</a>
+                    </div>
+                </div>
+                <div class'info'>
+                    ${overview || "No overview yet..."}
+                </div>
+            </div>
+        </div>
+    </div>
+ `;
+ return cardTemplate
 }
